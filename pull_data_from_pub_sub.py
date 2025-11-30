b713e818a80dbb5f5eb038e100b476643ce17832
Natalia Khaidanova <Khaidanova@cefetra.nl>
2025-11-30 17:07:24 +0100

    Made it less strict because it's streaming, not SQL ETL.

--- pull_data_from_pub_sub.py
+++ pull_data_from_pub_sub.py
@@ -1,6 +1,5 @@
 # GOAL: Pull events (messages) from Pub/Sub subscription and write the data to the BigQuery warehouse
-from google.cloud import pubsub_v1
-from google.cloud import bigquery
+from google.cloud import pubsub_v1, bigquery
 from dateutil.parser import parse
 from datetime import datetime
 import json
@@ -16,38 +15,52 @@ subscriber = pubsub_v1.SubscriberClient()
 subscription_path = subscriber.subscription_path(PROJECT_ID, SUBSCRIPTION_ID)
 bq_client = bigquery.Client(project=PROJECT_ID)
 
-#### FUNSTIONS ---------------------------------------------------------------------------------------------------------
+#### HELPER FUNCTIONS --------------------------------------------------------------------------------------------------
+# I've read that in streaming pipelines, we want to avoid "poison messages". This is different from SQL ETLs where we
+# set as strict rules as possible because the pipelines have restricted run times, and we can afford fixing every single
+# error. So, if a timestamp or numeric value is malformed, we set them to None instead of crashing and retrying forever
+def convert_to_timestamp(value):
+    try:
+        return parse(value).isoformat()
+    except Exception:
+        return None
+
+def convert_to_float(value):
+    try:
+        return float(value)
+    except Exception:
+        return None
+
+#### FUNCTIONS ---------------------------------------------------------------------------------------------------------
 def callback(event) -> None:
     print(f'Received event (message): {event.data}')
     try:
         data = json.loads(event.data.decode('utf-8'))
         print(f"Order ID: {data.get('order_id')}")
-        # Transform the data. This is needed mainly because BigQuery expects typed, structured rows and not JSON bytes.
-        # We must insure the correct data types since BigQuery expects them. (So, can't make everything TEXT like in
-        # PostgreSQL and then transform the data with a database procedure)
 
-        # Can set e.g., event_timestamp to None if we're unable to convert it to isoformat() or source to None if it
-        # doesn't exist in the data. But do we want it??? I think it depends on the streaming load and how important
-        # these data fields actually are. So, for now kept everything as simple as possible
+        # Transform the data. This is needed mainly because BigQuery expects typed, structured rows and not JSON bytes.
+        # We must ensure the correct data types since BigQuery expects them. (So, can't make everything TEXT like in
+        # PostgreSQL and then transform the data with a database procedure)
         row = {
             'order_id': data.get('order_id'),
             'event_type': data.get('event_type'),
-            'event_timestamp': parse(data.get('event_timestamp')).isoformat(),
-            'order_created_at': parse(data.get('order_created_at')).isoformat(),
+            'event_timestamp': convert_to_timestamp(data.get('event_timestamp')),
+            'order_created_at': convert_to_timestamp(data.get('order_created_at')),
             'customer_id': data.get('customer_id'),
-            'amount': float(data.get('amount')),
+            'amount': convert_to_float(data.get('amount')),
             'currency': data.get('currency'),
             'source': data.get('source'),
             'ingested_at': datetime.utcnow().isoformat() + 'Z'
         }
 
-        errors = bq_client.insert_rows_json(BQ_TABLE, [row]) # Will fail with 403 if you have a free tier
+        # Insert the row into BigQuery (Will fail with 403 if you have a free tier)
+        errors = bq_client.insert_rows_json(BQ_TABLE, [row])
         if errors:
             print(f'BigQuery insert failed: {errors}')
-            return # Don't ACK so Pub/Sub will retry the message
+            return  # Don't ACK so Pub/Sub will retry the message
 
         # ACK (remove the message from the Sub/Pub topic)
-        event.ack() # Comment out for testing
+        event.ack()  # Comment out for testing
 
     except Exception as e:
         print(f'Failed to process event (message): {e}')
@@ -56,11 +69,10 @@ def main():
     print(f'Listening for messages on: {subscription_path}')
     stream = subscriber.subscribe(subscription_path, callback=callback)
 
-    # Make the program keep running after you exit the terminal
     try:
         stream.result()
     except KeyboardInterrupt:
-        stream.cancel()
+        stream.cancel() # Make the program keep running after you exit the terminal
 
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
